602f0c11e38e13c3c21988331d8d65ae67515ac6
Aleksandr <Aleksandr2697@yandex.ru>
2024-03-22 13:12:07 +0300

    added an error handler

--- src/hooks/useGetStatistic.tsx
+++ src/hooks/useGetStatistic.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from 'react';
 import { getStatistic } from '../services/getStatistic';
 
 import { Valute } from '../services/type';
+import { error } from 'console';
 export const useGetStatistic = () => {
     const [valute, setValute] = useState<Valute>({});
 
     useEffect(() => {
-        getStatistic().then((res) => setValute(res.Valute));
+        getStatistic()
+            .then((res) => setValute(res.Valute))
+            .catch((error) => console.log(error));
     }, []);
 
     return { valute };
